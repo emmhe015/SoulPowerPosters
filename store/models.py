f144066fcdba048d1627f63d4671b27f0a60224b
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-22 14:30:29 +0000

    Create Profile class in models.py

--- store/models.py
+++ store/models.py
@@ -2,4 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_buyer = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
