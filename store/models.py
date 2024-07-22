bb8a4a27125b6534fe0631de9949f60c53d0d27e
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-22 14:32:50 +0000

    Create create_user_profile in models.py

--- store/models.py
+++ store/models.py
@@ -16,3 +16,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_buyer = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
