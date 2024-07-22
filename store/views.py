ad8851bfff31ed6f254c5aafa369ba0a05019f2a
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-22 14:46:54 +0000

    Insert imports to views.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegisterForm
 
 # Create your views here.
