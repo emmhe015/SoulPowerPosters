69456bcf5dfeb19a0afd4704b12eaa7362d60483
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-22 15:12:00 +0000

    Create urls.py in storeapp

--- posters_ecommerce/urls.py
+++ posters_ecommerce/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
     path('accounts/', include('allauth.urls')),
-    path('register/', register, name='register'),
+   
 ]
