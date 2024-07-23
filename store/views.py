5a2eed4c90c48401561eced55ea4ed273c8d2524
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-23 10:38:28 +0000

    Create add_to_cart view in views.py

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -43,5 +43,18 @@ def profile(request):
     }
 
     return render(request, 'store/profile.html', context)
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    messages.success(request, f"{product.name} was added to your cart.")
+    return redirect('product_detail', product_id=product.id)
 
 
