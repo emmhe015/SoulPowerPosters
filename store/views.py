b002518b59a31acc5af4a9c7c398e2b5947ce0e3
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-23 10:37:08 +0000

    Insert imports from models to views.py

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm
+from .models import Product, Cart, CartItem
 
 
 def home(request):
