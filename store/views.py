1581891dafcdd161f6b73d85c3ba7e789b6c4a33
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-23 09:19:17 +0000

    Insert imports to views.py

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 
 def home(request):
     return render(request, 'store/home.html')
