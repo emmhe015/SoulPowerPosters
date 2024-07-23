d61b95dd37a7b9cd3d749449d5509c3880ac4192
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-23 09:22:36 +0000

    Create Updateform to user and profile in forms.py

--- store/forms.py
+++ store/forms.py
@@ -15,3 +15,15 @@ class UserRegisterForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['email', 'password', 'username',]
