b4dfa0e2841d224b62559ba93c122eaa428744b1
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-23 10:13:17 +0000

    Update register def in views.py

--- store/views.py
+++ store/views.py
@@ -17,7 +17,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
     else:
         form = UserRegisterForm()
     return render(request, 'store/register.html', {'form': form})
