89a8efad2fa3ba0ff4e32d2890529f81c7bf13d3
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-23 10:22:05 +0000

    Remove ProfileUpdateForm from views.py

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm
 
 
 def home(request):
