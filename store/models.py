70df9263acf52749edf38724ef4f7a4332b32164
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-23 10:34:37 +0000

    Create Product class in models.py

--- store/models.py
+++ store/models.py
@@ -18,3 +18,11 @@ class Profile(models.Model):
     is_admin = models.BooleanField(default=False)
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
