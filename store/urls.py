f889591db134403e7fc735599ad366f75e164a6d
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-23 08:58:11 +0000

    Create login.html

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import register, home
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
     path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='store/logout.html'), name='logout'),
 ]
