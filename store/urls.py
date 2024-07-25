927d744768d4cdbf561077fa01d6b85ad6f55282
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-25 13:44:40 +0000

    Add product_list to urls.py

--- store/urls.py
+++ store/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('cart/', view_cart, name='view_cart'),
     path('cart/add/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/remove/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
-    path('products/<int:product_id>/', product_detail, name='product_detail'),
+    path('products/', store_views.product_list, name='product_list'),
 
 ]
