636ee7877e32a81265e9c351921d157627e02939
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-27 17:25:07 +0000

    Add string to product_id in views in cart views

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import view_cart, add_to_cart, remove_from_cart
+from .views import view_cart, add_to_cart, remove_from_cart, update_cart
+
+app_name = 'cart'
 
 urlpatterns = [
     path('cart/', view_cart, name='view_cart'),
     path('cart/add/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/remove/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
+    path('cart/update/<int:product_id>/', update_cart, name='update_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -15,13 +15,12 @@ def view_cart(request):
 
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    
     cart = request.session.get('cart', {})
-    
-    if product_id in cart:
-        cart[product_id]['quantity'] += 1
+
+    if str(product_id) in cart:
+        cart[str(product_id)]['quantity'] += 1
     else:
-        cart[product_id] = {
+        cart[str(product_id)] = {
             'name': product.name,
             'price': str(product.price),
             'quantity': 1,
@@ -37,18 +36,17 @@ def update_cart(request, product_id):
     if request.method == 'POST':
         cart = request.session.get('cart', {})
         quantity = int(request.POST.get('quantity', 1))
-        if product_id in cart:
+        if str(product_id) in cart:
             if quantity > 0:
-                cart[product_id]['quantity'] = quantity
+                cart[str(product_id)]['quantity'] = quantity
             else:
-                del cart[product_id]
-        request.session['cart'] = cart
-    return redirect('view_cart')
+                del cart[str(product_id)]
+    return redirect('cart:view_cart')
 
 def remove_from_cart(request, item_id):
     cart = request.session.get('cart', {})
-    if product_id in cart:
-        del cart[product_id]
+    if str(product_id) in cart:
+        del cart[str(product_id)]
     request.session['cart'] = cart
     messages.success(request, "Item was removed from your cart.")
     return redirect('view_cart')
