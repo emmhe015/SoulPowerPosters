5e17c01add6957da6c70a6ada66c6125c69ce979
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-27 16:46:19 +0000

    Initial migration for cart

--- cart/models.py
+++ cart/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
