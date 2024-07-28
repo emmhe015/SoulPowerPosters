54a8dcd15098df9a27396e12591eb78cd98b13f1
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-28 09:27:27 +0000

    Add toast messages to views.py in cart

--- cart/views.py
+++ cart/views.py
@@ -19,6 +19,7 @@ def add_to_cart(request, product_id):
 
     if str(product_id) in cart:
         cart[str(product_id)]['quantity'] += 1
+        messages.success(request, f'Updated quantity of {product.name} in your cart.')
     else:
         cart[str(product_id)] = {
             'name': product.name,
@@ -26,7 +27,7 @@ def add_to_cart(request, product_id):
             'quantity': 1,
             'image_url': product.image.url if product.image else '',
         }
-    
+        messages.success(request, f'Added {product.name} to your cart.')
    
     request.session['cart'] = cart
     
@@ -39,14 +40,21 @@ def update_cart(request, product_id):
         if str(product_id) in cart:
             if quantity > 0:
                 cart[str(product_id)]['quantity'] = quantity
+                messages.success(request, f'Updated quantity of {cart[str(product_id)]["name"]} in your cart.')
             else:
                 del cart[str(product_id)]
+                messages.success(request, 'Removed item from your cart.')
+        else:
+            messages.error(request, 'Item not found in your cart.')
+        request.session['cart'] = cart
     return redirect('cart:view_cart')
 
 def remove_from_cart(request, product_id):
     cart = request.session.get('cart', {})
     if str(product_id) in cart:
         del cart[str(product_id)]
+        messages.success(request, 'Item was removed from your cart.')
+    else:
+        messages.error(request, 'Item not found in your cart.')
     request.session['cart'] = cart
-    messages.success(request, "Item was removed from your cart.")
     return redirect('cart:view_cart')
