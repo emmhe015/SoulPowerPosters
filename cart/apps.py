8e0cb065136c8a0b096532a1514c8a8e2c7b9dee
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-28 16:37:43 +0000

    Update content in apps.py

--- cart/apps.py
+++ cart/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
-class CartConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cart'
+class CheckoutConfig(AppConfig):
+    name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
