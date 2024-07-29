c9000998c36dc966a0c45e5a3a1fec7850274174
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-29 09:24:35 +0000

    Add checkout urls to project urls.py

--- posters_ecommerce/urls.py
+++ posters_ecommerce/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
 
 if settings.DEBUG:
