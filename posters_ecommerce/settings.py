eeb4617843b3bd17fcb95bd2d0f56235dc462a22
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-29 12:07:13 +0000

    update settings.py

--- posters_ecommerce/settings.py
+++ posters_ecommerce/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
+
+import env
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
