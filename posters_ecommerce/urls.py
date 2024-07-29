d122de608e52bfc6ea0c05650729ef75c68d4a90
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-29 18:05:26 +0000

    Create profiles.html

--- posters_ecommerce/urls.py
+++ posters_ecommerce/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profiles/', include('profiles.urls')),
 ]
 
 if settings.DEBUG:
