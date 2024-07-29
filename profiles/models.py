837f6d6798bc507443b0165851a9f0de94aac43e
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-29 17:48:09 +0000

    Add profile models

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    """
+    Profile model to extend the built-in User model with additional attributes.
+
+    Attributes:
+        user (OneToOneField): A one-to-one relationship with the Django User model.
+        is_buyer (BooleanField): A flag indicating whether the user is a buyer. Defaults to True.
+        is_admin (BooleanField): A flag indicating whether the user is an admin. Defaults to False.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    is_buyer = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+@receiver(post_save, sender=User)
+def create_or_update_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        Profile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.profile.save()
