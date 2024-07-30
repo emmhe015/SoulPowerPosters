2e2118c13e17bb45106eefafe25f3a8e6776bd78
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-30 08:48:56 +0000

    Update settings

--- posters_ecommerce/settings.py
+++ posters_ecommerce/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(b)j%!@7g$5n+yh6ty9vqy0frb@-(984!4$lp1*=#5*+sqvpb$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-emmhe015-soulpowerposte-7hc55ptd2bs.ws.codeinstitute-ide.net', 'soulpowerposters-862debca0bd9.herokuapp.com']
 
