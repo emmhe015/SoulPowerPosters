67ca19eefeaeeea5cf7532c48c73fc7d0392979d
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-30 06:39:16 +0000

    Update links to edit, add and delete products

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -10,8 +9,7 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
 
     image = forms.ImageField(label='Image',
-                             required=False,
-                             widget=CustomClearableFileInput)
+                             required=False,)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
