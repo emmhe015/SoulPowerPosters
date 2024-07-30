5231065836223fed31495a38f3166d8dd444964a
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-30 06:00:36 +0000

    Update product admin views and urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import product_list, product_detail, all_products
+from .views import product_list, product_detail, all_products, add_product, edit_product, delete_product
 
 app_name = 'products'
 
@@ -7,4 +7,9 @@ urlpatterns = [
     path('', all_products, name='products'),
     path('products/', product_list, name='product_list'),
     path('products/<int:pk>/', product_detail, name='product_detail'),
+    path('add/', add_product, name='add_product'),
+    path('edit/<int:product_id>/', edit_product, name='edit_product'),
+    path('delete/<int:product_id>/',
+         delete_product,
+         name='delete_product'),
 ]
