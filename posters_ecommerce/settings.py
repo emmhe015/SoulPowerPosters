c77d52a6190b40f7f110843086bfec22b682272e
Emma Henriksson <emmaha.eh@gmail.com>
2024-08-22 08:49:50 +0000

    Change setting for secretKey

--- posters_ecommerce/settings.py
+++ posters_ecommerce/settings.py
@@ -10,12 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-
 import dj_database_url
 from pathlib import Path
-
-if os.path.isfile('env.py'):
- import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-emmhe015-soulpowerposte-7hc55ptd2bs.ws.codeinstitute-ide.net', 'soulpowerposters-862debca0bd9.herokuapp.com']
 
